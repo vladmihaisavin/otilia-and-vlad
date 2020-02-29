bbe670a34c1b1e0d46bdad4946b698f53da94bcd
Vlad's Mac <savin.vladmihai@gmail.com>
2020-02-29 11:35:22 +0200

    added first components

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { useAuth0 } from "../../react-auth0-spa"
+import { Link } from "react-router-dom"
+
+const NavBar = () => {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0()
+
+  return (
+    <div>
+      {!isAuthenticated && (
+        <button onClick={() => loginWithRedirect({})}>Log in</button>
+      )}
+
+      {isAuthenticated && (
+        <span>
+          <button onClick={() => logout()}>Log out</button>
+          <Link to="/">Home</Link>&nbsp;
+          <Link to="/profile">Profile</Link>
+        </span>
+      )}
+    </div>
+  )
+}
+
+export default NavBar
